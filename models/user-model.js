4d10c8d1ea0d1470cb48eb7dc78a6cc6826b3fb6
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2018-09-21 15:44:04 +0200

    Add auth stuff

---
+++ models/user-model.js
@@ -0,0 +1,42 @@
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+  // document structure & rules
+  fullName: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^.+@.+\..+$/,
+  },
+  encryptedPassword: { type: String },
+  role: {
+    type: String,
+    enum: [ "normal", "admin" ],
+    required: true,
+    default: "normal",
+  },
+  avatar: {
+    type: String,
+    default: "/images/avatar.png",
+  }
+}, {
+  // additional settings for Schema constructor function (class)
+  timestamps: true,
+});
+
+// define the "isAdmin" virtual property (a property that's really a method)
+// CAN'T be an arrow function because it uses "this"
+// (we use this to get around the limits on conditions in HBS files)
+userSchema.virtual("isAdmin").get(function () {
+  return this.role === "admin";
+});
+
+const User = mongoose.model("User", userSchema);
+
+
+module.exports = User;
---
+++ routes/auth-router.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user-model.js");
+
+const router = express.Router();
+
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth-views/signup-form.hbs");
+});
+
+router.post("/process-signup", (req, res, next) => {
+  const { fullName, email, originalPassword } = req.body;
+
+  // encrypt the submitted password
+  const encryptedPassword = bcrypt.hashSync(originalPassword, 10);
+
+  User.create({ fullName, email, encryptedPassword })
+    .then(userDoc => {
+      // save a flash message to display in the HOME page
+      req.flash("success", "Sign up success! 🖖🏾");
+      res.redirect("/");
+    })
+    .catch(err => next(err));
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("auth-views/login-form.hbs");
+});
+
+router.post("/process-login", (req, res, next) => {
+  const { email, originalPassword } = req.body;
+
+  // first check to see if there's a document with that email
+  User.findOne({ email: { $eq: email } })
+    .then(userDoc => {
+      // "userDoc" will be empty if the email is wrong (no document in database)
+      if (!userDoc) {
+        // save a flash message to display in the LOGIN page
+        req.flash("error", "Incorrect email. 🤦‍♂️");
+        res.redirect("/login");
+        return; // use "return" instead of a big "else {}"
+      }
+
+      // second check the password
+      const { encryptedPassword } = userDoc;
+      // "compareSync()" will return false if the "originalPassword" is wrong
+      if (!bcrypt.compareSync(originalPassword, encryptedPassword)) {
+        // save a flash message to display in the LOGIN page
+        req.flash("error", "Password is wrong. ️🤯");
+        res.redirect("/login");
+        return;
+      }
+
+      // LOG IN THIS USER
+      // "req.logIn()" is a Passport method that calls "serializeUser()"
+      // (that saves the USER ID in the session)
+      req.logIn(userDoc, () => {
+        // save a flash message to display in the HOME page
+        req.flash("success", "Log in success! 🧙‍♀️");
+        // go to the home page if password is GOOD (log in worked!)
+        res.redirect("/");
+      });
+    })
+    .catch(err => next(err));
+});
+
+router.get("/logout", (req, res, next) => {
+  // "req.logOut()" is a Passport method that removes the user ID from session
+  req.logOut();
+
+  // save a flash message to display in the HOME page
+  req.flash("success", "Logged out successfully!");
+  res.redirect("/");
+});
+
+
+module.exports = router;
